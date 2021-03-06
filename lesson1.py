1d40e412056366df6d6efd80c33ac0ed0d86ef2d
Albertpython <abo.zakaryan.97@mail.ru>
2021-03-06 17:11:22 +0400

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+# print("Hello world")
+
+# name = 'Mary'
+# print(name)
+# name = input("What is your name?")
+# age = input("How old are you?")
+# phone = input("Phone:")
+# email = input("Please mention your email addres:")
+# print('Hello',name, 'age',age,'phone', phone,'email',email)
+# user = int(input('Erb es cnvel?'))
+# print(2021-user)
+# print('5' + '5')
+# a = int(input("erk "))
+# b = int(input("layn "))
+# s = a * b
+# print(s)
+
+
+
+
+# print('Tell me something boy\n\t are you happy in this modern\n world?')
+# a = int(input("koxm "))
+# h = int(input("bardrutyun "))
+# s = 0.5 * a * h
+# print(s)
+print(0.1+0.2)
